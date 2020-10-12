dda23a2f2274ebc3bb5d8fddba9a8d85c1799666
osamudiamen <osas.azamegbe@gmail.com>
2020-10-12 23:56:12 +0100

    created useEffect and useState for fetching users urls from backend.

--- src/components/pages/profile/Profile.js
+++ src/components/pages/profile/Profile.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Profile.css'
 import Form from '../../Form'
 import List from '../../List'
 
 
-const Profile = ({Authorization}) => {
+const Profile = ({user}) => {
+
+    const [urls, setUrls] = useState([])
+
+    useEffect(() => {
+        
+        const getUrls = async () => {
+            const loggedInUser = localStorage.getItem("user");
+            if (loggedInUser) {
+
+                const foundUser = JSON.parse(loggedInUser);
+                const token = foundUser.token
+                const url = `http://localhost:5000/api/v1/shorten`
+                const response = await fetch(url,{
+                    method: "GET",
+                    headers: {
+                        Authorization: token
+                    }
+                })
+
+                const data = await response.json()
+                console.log(data)
+                if (data.urls) setUrls(data.urls)
+            }   
+        }
+
+        getUrls()
+
+    },[])
+
+
+
     return (
         <div className="profile">
             <div className="profile-container">
-                <h1>Welcome, User</h1>
+                <h1>Welcome, {user.username}</h1>
                 <Form/>
                 <List 
                 heading="Url List"
-                items={[1,2,3,4]}/>
+                items={urls}/>
             </div>            
         </div>
     )
