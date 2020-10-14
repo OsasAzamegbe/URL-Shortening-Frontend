000d6e87972a708d7f281a34f405939d7018275a
osamudiamen <osas.azamegbe@gmail.com>
2020-10-14 19:17:54 +0100

    created Home component and styled Logout component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignUp from './components/pages/signup/SignUp'
 import Profile from './components/pages/profile/Profile'
 import Login from './components/pages/login/Login'
 import Logout from './components/pages/logout/Logout'
+import Home from './components/pages/home/Home'
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
       <Router>
       <Navbar user={user} />
       <Switch>
+        <Route path='/app' exact component={Home}/>
         <Route path='/app/profile' exact render={(props) => <Profile {...props} user={user} />}/>
         <Route path='/app/sign-up' exact component={SignUp}/>
         <Route path='/app/login' exact render={(props) => <Login {...props} setUser={setUser}/>}/>
