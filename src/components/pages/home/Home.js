c0efb7411658be46342a76077661f5c68a27b4b2
osamudiamen <osas.azamegbe@gmail.com>
2020-10-14 19:27:29 +0100

    Styled Home component. edited styling for Button component.

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
+import Button from '../../Button'
 
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <h1>Home Page</h1>
+        <div className="home">
+            <div className="home-container">
+                <h1>Home Page</h1>
+                <Button
+                children="Sign Up"/>
+            </div>
         </div>
     )
 }
