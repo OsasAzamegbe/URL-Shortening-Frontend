a0cc97b1df911a0190cb90cd8c1a2a57a602fcd1
osamudiamen <osas.azamegbe@gmail.com>
2020-10-14 13:41:44 +0100

    added Logout component to App as route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import SignUp from './components/pages/signup/SignUp'
 import Profile from './components/pages/profile/Profile'
 import Login from './components/pages/login/Login'
+import Logout from './components/pages/logout/Logout'
 
 
 function App() {
@@ -25,9 +26,10 @@ function App() {
       <Router>
       <Navbar user={user} />
       <Switch>
-        <Route path='/profile' render={(props) => <Profile {...props} user={user} />}/>
-        <Route path='/sign-up' component={SignUp}/>
-        <Route path='/login' render={(props) => <Login {...props} setUser={setUser}/>}/>
+        <Route path='app/profile' exact render={(props) => <Profile {...props} user={user} />}/>
+        <Route path='app/sign-up' exact component={SignUp}/>
+        <Route path='app/login' exact render={(props) => <Login {...props} setUser={setUser}/>}/>
+        <Route path='app/logout' exact component={Logout}/>
       </Switch>
     </Router>
     </div>
---
+++ src/components/pages/logout/Logout.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react'
+import './Logout.css'
+
+
+const Logout = () => {
+
+    useEffect()
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Logout
