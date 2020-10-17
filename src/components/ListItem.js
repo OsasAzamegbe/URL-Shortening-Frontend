ce574a8a96af1cd016fc1745093e4a7791df44e8
osamudiamen <osas.azamegbe@gmail.com>
2020-10-17 17:45:21 +0100

    styled ListItem and List components and included link and showing created date

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './ListItem.css'
+import {Link} from 'react-router-dom'
 
 
 const ListItem = ({item}) => {
 
     return (
-        <div className="item">
-            <li className={`item-item `} >{item.shortUrl}</li>
-        </div> 
+        <Link 
+        to={item.url.slice(6)}
+        target="_blank">
+            <div className="item">
+                <li>
+                    <p className="item-text" >{item.shortUrl}</p>
+                    <p className="item-subtext">
+                        {`created: ${item.dateCreated}`}
+                    </p>
+                </li>
+            </div> 
+        </Link>
+        
     );
 };
 
