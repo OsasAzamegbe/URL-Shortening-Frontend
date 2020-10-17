8f1bec5121c8edd27a4f08c16e1b42eeeff98ee4
osamudiamen <osas.azamegbe@gmail.com>
2020-10-17 18:15:06 +0100

    Added and stlyed copy icon

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './ListItem.css'
 import {Link} from 'react-router-dom'
+import {FaCopy} from 'react-icons/fa'
 
 
 const ListItem = ({item}) => {
+    const urlTextRef = useRef(null)
 
     return (
-        <Link 
-        to={item.url.slice(6)}
-        target="_blank">
-            <div className="item">
-                <li>
-                    <p className="item-text" >{item.shortUrl}</p>
-                    <p className="item-subtext">
-                        {`created: ${item.dateCreated}`}
-                    </p>
-                </li>
-            </div> 
-        </Link>
+        <div className="row">
+            <Link 
+            to={item.url.slice(6)}
+            target="_blank">
+                <div className="item">
+                    <li>
+                        <p className="item-text" >{item.shortUrl}</p>
+                        <p className="item-subtext">
+                            {`created: ${item.dateCreated}`}
+                        </p>
+                    </li>
+                </div> 
+            </Link>
+            <div  className="copy-icon">
+            <FaCopy/>
+            </div>
+        </div>
+        
         
     );
 };
