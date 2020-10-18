c9a996ab681a6c769119bec75d6c17af6520b215
osamudiamen <osas.azamegbe@gmail.com>
2020-10-18 22:53:38 +0100

    modified fonts for HomeSection component.

--- src/components/pages/home/Data.js
+++ src/components/pages/home/Data.js
@@ -5,7 +5,7 @@ const sectionOneObj = {
     lightTextDesc: false,
     topLine: "Shorten Links Expeditiously",
     headLine: "Start Today!",
-    description: "Welcome to Shorten! This is a URL shortnening service for creating short links that are highly available on the fly!",
+    description: "Welcome to Shorten. This is a URL shortening service for creating short links, that are highly available, on the fly!",
     buttonLabel: "Create A Free Account",
     imgStart: false,
     img: "images/svg-1.svg",
