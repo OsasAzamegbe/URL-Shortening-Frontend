b20a324aa20ee3b938e228025b68f84d758ca99b
osamudiamen <osas.azamegbe@gmail.com>
2020-10-18 22:07:28 +0100

    created HomeSection component and added it to home component.

---
+++ src/components/HomeSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Button from './Button'
+import './HomeSection.css'
+
+
+const HomeSection = ({
+    lightBg, topLine, lightText, lightTextDesc, headLine, description, buttonLabel, img, alt, imgStart, hideButton
+}) => {
+
+    return(
+        <div className={lightBg ? 'home__home-section' : 'home__home-section darkBg'}>
+            <div className="container">
+                <div 
+                className="row home__home-row"
+                style={{display: 'flex', flexDirection: imgStart ? 'row-reverse' : 'row'}}>
+                    <div className="col">
+                        <div className="home__home-text-wraper">
+                            <div className="top-line">{topLine}</div>
+                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headLine}</h1>
+                            <p className={lightTextDesc ? 'home__home-subtitle' : 'home__home-subtitle dark'}>{description}</p>
+                            {!hideButton ? 
+                            <Link to="/app/sign-up">
+                                <Button buttonSize='btn--wide' buttonColor='red' >{buttonLabel}</Button>
+                            </Link> : ""
+                            }
+
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="home__home-img-wrapper">
+                            <img className="home__home-img" src={img} alt={alt}/> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeSection;
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Home.css'
-import Button from '../../Button'
+import HomeSection from '../../HomeSection'
 
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home-container">
-                <h1>Home Page</h1>
-                <Button
-                children="Sign Up"/>
+                <HomeSection
+                buttonLabel={"Create A Free Account"}
+                lightBg={true}
+                topLine={"Shorten Links Expeditiously"} 
+                lightText={false} 
+                lightTextDesc={false} 
+                headLine={"Start Today!"} 
+                description={"Welcome to Shorten! This is a URL shortnening service for creating short links that are highly available on the fly!"} img={"images/svg-1.svg"} alt={"Home page illustration"} 
+                imgStart={false} hideButton={false}
+                />
             </div>
         </div>
     )
